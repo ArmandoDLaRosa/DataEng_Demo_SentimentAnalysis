5f3db845c227a7839a0588fe6d05187701352bcb
armando <a01570376@itesm.mx>
2022-06-09 01:25:08 -0500

    unlfs

---
+++ src/extractions/trivago.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import time
---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""
+_summary_:
+    Simple orchaestration
+"""
+
+def main():
+    """
+    _summary_
+    """    
+    pass
+
+if __name__ == "__main__":
+  main()
